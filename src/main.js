0513a97e90aca3749c0de5c4ff95e368b85586cd
wukui <774553007@qq.com>
2020-08-14 19:46:56 +0800

    内容持续更新中

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from "./router/router"
+import echarts from "echarts"
+Vue.prototype.$echarts = echarts;
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+//创建一个全局bus
+Vue.prototype.$bus=new Vue();
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
